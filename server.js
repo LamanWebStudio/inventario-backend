77f346e01ac893f3eced8f0ffc43fe041d5975d7
Jhonatan Castro | Desarrollador Web <168943236+RouceDevStudio@users.noreply.github.com>
2025-06-01 01:28:51 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
 const app = express();
-const FRONTEND_URLS =['https://inventario-phi.vercel.app', "https://rousedevstudio.vercel.app"];
+const FRONTEND_URLS =['https://inventario-phi.vercel.app', "https://roucedevstudio.vercel.app"];
 
 app.use(cors({
   origin: function (origin, callback) {
